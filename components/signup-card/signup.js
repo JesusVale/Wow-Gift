3ec4bbc220d05f96fdffdff44a06bfa0597ad242
JesusVale <108861698+JesusVale@users.noreply.github.com>
2023-11-13 17:18:53 -0700

    create signup card

---
+++ components/signup-card/signup.js
@@ -0,0 +1,57 @@
+import { registrarUsuario } from "../../services/tienda-regalos.js";
+
+export default class SignupCard extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        const shadow = this.attachShadow({mode: "open"})
+        this.#render(shadow)
+    }
+
+    async #render(shadow){
+        try{
+            const response = await fetch("./components/signup-card/signup-card.html");
+            const html = await response.text();
+            shadow.innerHTML = html;
+            this.#eventSignup(shadow)
+        } catch(error){
+            console.log("error Loading html" + error)
+        }
+    }
+
+    async #eventSignup(shadow){
+        const form = shadow.querySelector(".signup__form");
+        form.addEventListener("submit", async (e) =>{
+            e.preventDefault();
+            const toast = shadow.querySelector("toast-component");
+            const nombre = shadow.querySelector('[name="nombre"]').getValue();
+            const email = shadow.querySelector('[name="email"]').getValue();
+            const password = shadow.querySelector('[name="password"]').getValue();
+            const confirm = shadow.querySelector('[name="confirm"]').getValue();
+            const telefono = shadow.querySelector('[name="telefono"]').getValue();
+
+            if(password != confirm){
+                //TODO Mostrar mensaje - Las contraseñas no coinciden
+                toast.showToast("Las contraseñas no coinciden")
+                console.log("ea")
+                return;
+            }
+
+            const info = await registrarUsuario({nombre, tipo:"Usuario", email, password, telefono});
+            if(info.msg){
+                //TODO Mostrar mensaje de error
+                toast.showToast(info.msg)
+                console.log(info.msg)
+                return;
+            }
+            //Mostrar Mensaje - Registrado con éxito
+            toast.showToast("Usuario Registrado Correctamente", "success", () =>{
+                window.location.href = "/index.html"
+            }, 2000)
+            
+
+        })
+    }
+}
--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ import HeaderComponent from "../components/header-component/header-component.js"
 import CategoryNav from "../components/category-nav/category-nav.js";
 import InputSign from "../components/input-sign/input.js";
 import LoginCard from "../components/login-card/login.js";
+import SignupCard from "../components/signup-card/signup.js";
 import ToastComponent  from "../components/toast/toast.js";
 
 customElements.define("header-component", HeaderComponent);
@@ -9,3 +10,4 @@ customElements.define("category-nav", CategoryNav);
 customElements.define("toast-component", ToastComponent);
 customElements.define("input-sign", InputSign);
 customElements.define("login-card", LoginCard);
+customElements.define("signup-card", SignupCard);
