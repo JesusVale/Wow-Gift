72fa2b08127921d0f5517d7cb7fd12c912c24942
JesusVale <108861698+JesusVale@users.noreply.github.com>
2023-11-13 17:10:14 -0700

    create login card

---
+++ components/login-card/login.js
@@ -0,0 +1,44 @@
+import { login } from "../../services/tienda-regalos.js";
+
+export default class LoginCard extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        const shadow = this.attachShadow({mode: "open"})
+        this.#render(shadow)
+    }
+
+    async #render(shadow){
+        try{
+            const response = await fetch("./components/login-card/login.html");
+            const html = await response.text();
+            shadow.innerHTML = html;
+            this.#eventLogin(shadow);
+        } catch(error){
+            console.log("error Loading html" + error)
+        }
+    }
+
+    async #eventLogin(shadow){
+        const form = shadow.querySelector(".login__form");
+        form.addEventListener("submit", async (e) =>{
+            e.preventDefault();
+            const toast = shadow.querySelector("toast-component");
+            const email = shadow.querySelector('[name="email"]').getValue();
+            const password = shadow.querySelector('[name="password"]').getValue();
+            const info = await login(email, password);
+            if(info.msg){
+                toast.showToast(info.msg)
+                return;
+            }
+            sessionStorage.setItem("token", info.token);
+            localStorage.setItem("user", info.usuario.nombre);
+            localStorage.setItem("type", info.usuario.tipo);
+            window.location.href = "/index.html"
+
+        })
+    }
+}
+
--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 import HeaderComponent from "../components/header-component/header-component.js";
 import CategoryNav from "../components/category-nav/category-nav.js";
+import InputSign from "../components/input-sign/input.js";
+import LoginCard from "../components/login-card/login.js";
 
 customElements.define("header-component", HeaderComponent);
 customElements.define("category-nav", CategoryNav);
+customElements.define("input-sign", InputSign);
+customElements.define("login-card", LoginCard);
