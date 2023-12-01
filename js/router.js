3484046f25829d677376c37f829f714d817829c3
Áulus Pinho <aulus97@gmail.com>
2023-12-01 15:12:09 -0700

    Sorry again but trust me now router.js is like it was before my branch haha

--- js/router.js
+++ js/router.js
@@ -33,12 +33,6 @@ const PATHS = [
 		title: "Wow Gift | Envio",
 		name: "shipping",
 		type: "sesion"
-	},
-    {
-		path: "/feedback",
-		title: "Wow Gift | Reseña",
-		name: "feedback",
-		type: "sesion"
 	}
 
 ]
