85b78af21710e34584f57d4d0c09f60df6492fa7
JesusVale <108861698+JesusVale@users.noreply.github.com>
2023-12-05 17:35:33 -0700

    update header

--- components/header-component/header-signin.js
+++ components/header-component/header-signin.js
@@ -41,16 +41,7 @@ export const headerSignIn = `<link rel="stylesheet" href="/components/header-com
             </span>
             <ul class="menu-cuenta__list">
                 <li class="menu-cuenta__item">
-                    <a href="#" class="menu-cuenta__link">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
-                         </svg> Mi Cuenta
-                    </a>
-                </li>
-                <li class="menu-cuenta__item">
-                    <a href="#" class="menu-cuenta__link">
+                    <a href="/shopping" class="menu-cuenta__link">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-bag" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M6.331 8h11.339a2 2 0 0 1 1.977 2.304l-1.255 8.152a3 3 0 0 1 -2.966 2.544h-6.852a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304z"></path>
@@ -83,7 +74,7 @@ export const headerSignIn = `<link rel="stylesheet" href="/components/header-com
     
             </ul>
         </nav>
-        <a href="/compras" class="header__link">Mis Compras</a>
+        <a href="/shopping" class="header__link">Mis Compras</a>
         <cart-component></cart-component>
     </div>
 
